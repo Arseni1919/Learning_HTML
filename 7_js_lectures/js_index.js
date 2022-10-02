60bde967ebd2d37434d23caf6adb246557bcfeab
perchik_air <1919ars@gmail.com>
2022-10-02 11:15:13 +0300

    more

--- 7_js_lectures/js_index.js
+++ 7_js_lectures/js_index.js
@@ -174,22 +174,107 @@
 // Person.prototype.getFullName = function () {
 //         return `${this.firstName} ${this.lastName}`
 // }
-class Person {
-    constructor(firstName, lastName, dob) {
-        this.firstName = firstName
-        this.lastName = lastName
-        this.dob = new Date(dob)
-    }
-    getBirthYear() {
-        return this.dob.getFullYear()
-    }
-    getFullName() {
-        return `${this.firstName} ${this.lastName}`
+// class Person {
+//     constructor(firstName, lastName, dob) {
+//         this.firstName = firstName
+//         this.lastName = lastName
+//         this.dob = new Date(dob)
+//     }
+//     getBirthYear() {
+//         return this.dob.getFullYear()
+//     }
+//     getFullName() {
+//         return `${this.firstName} ${this.lastName}`
+//     }
+// }
+// const person1 = new Person('Moti', 'Katz', '4-3-1980')
+// console.log(person1)
+// console.log(person1.getBirthYear())
+// console.log(person1.getFullName())
+// console.log(person1.dob.getFullYear())
+
+// console.log(window)
+
+// alert()
+// window.alert()
+// window.console.log('aaa')
+// console.log(window.innerWidth)
+// console.log(window.innerHeight)
+// console.log(window.localStorage)
+// console.log(window.document)
+
+//single element
+// console.log(document.getElementById('my-form'))
+// const form = document.getElementById('my-form')
+// console.log(form)
+
+// console.log(document.querySelector('#my-form'))
+// console.log(document.querySelector('.block'))
+// console.log(document.querySelector('form'))
+//
+// console.log(document.getElementsByClassName('item'))
+// console.log(document.getElementsByTagName('li'))
+// console.log(document.querySelectorAll('.item'))
+
+// const items = document.querySelectorAll('.item')
+// items.forEach((item) => console.log(item))
+
+// const ul = document.querySelectorAll('.items')[0]
+// const ul = document.querySelector('.items')
+// console.log(ul)
+// // ul.remove()
+// ul.lastElementChild.remove()
+// ul.firstElementChild.textContent = 'Hello'
+// ul.children[1].innerText = 'Brad'
+// ul.lastElementChild.innerHTML = '<h1>Hello</h1>'
+
+// btn.style.background = 'red'
+// const btn = document.querySelector('.btn')
+//
+// btn.addEventListener('mouseover', (e) => {
+//     e.preventDefault()
+//     document.querySelector('#my-form').style.background = '#ccc'
+//     document.querySelector('body').classList.add('bg-dark')
+//     document.querySelector('.items').lastElementChild.innerHTML = '<h1>Hello</h1>'
+// })
+//
+//
+// btn.addEventListener('mouseout', (e) => {
+//     e.preventDefault()
+//     document.querySelector('#my-form').style.background = '#ccc'
+//     document.querySelector('body').classList.add('bg-dark')
+//     document.querySelector('.items').lastElementChild.innerHTML = '<h1>Hello</h1>'
+// })
+
+const myForm = document.querySelector('#my-form')
+const nameInput = document.querySelector('#name')
+const emailInput = document.querySelector('#email')
+const msg = document.querySelector('.msg')
+const userList  = document.querySelector('.users')
+
+const onSubmit = (e) => {
+    e.preventDefault()
+    if (nameInput.value === '' || emailInput.value === ''){
+        msg.innerHTML = 'Please enter all fields'
+        msg.classList.add('error')
+        setTimeout(() => {
+            msg.innerHTML = ''
+            msg.classList.remove('error')
+        }, 1000)
+    } else {
+        const li = document.createElement('li')
+        li.appendChild(document.createTextNode(`${nameInput.value}: ${emailInput.value}`))
+        userList.appendChild(li)
+        // Clear fields
+        nameInput.value = ''
+        emailInput.value = ''
     }
 }
-const person1 = new Person('Moti', 'Katz', '4-3-1980')
-console.log(person1)
-console.log(person1.getBirthYear())
-console.log(person1.getFullName())
+myForm.addEventListener('submit', onSubmit)
 
-// console.log(person1.dob.getFullYear())
+
+
+
+
+
+
